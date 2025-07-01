f7c1bbfaed6a3e4aa0f61123790349c4326648e5
GrootProgrammer <ryan.groot+github@gmail.com>
2025-07-01 12:31:42 +0200

    add gsvi with optimistic value iteration

--- src/model_checker/gsvi.py
+++ src/model_checker/gsvi.py
@@ -18,41 +18,85 @@ def gsvi(mmodel, max_iterations, precision):
     results = {p.name: PropertyResult(PropertyResultType.NOT_SUPPORTED, None, 0) for p in properties_ if not p.is_valid}
     for prop in properties:
         timer = time.time()
-        try:
-            results[prop.name] = gsvi_thread(prop, timer, max_iterations, precision)
-        except Exception as e:
-            results[prop.name] = PropertyResult(PropertyResultType.ERROR, None, time.time() - timer)
+        #try:
+        results[prop.name] = gsvi_thread(prop, timer, max_iterations, precision)
+        #except Exception as e:
+        #    results[prop.name] = PropertyResult(PropertyResultType.ERROR, None, time.time() - timer)
     return results
 
 def gsvi_thread(prop, timer, max_iterations, precision):
-    # all states
     S = prop.get_states()
-    # all states that satisfy the goal
-    G = set([s for s in S if prop.get_goal_value(s) == True])
-    if __debug__:
-        assert len(G) > 0
-    # all states that are not safe
-    S0 = set([s for s in S if prop.is_safe(s) == False or len(prop.get_actions(s)) == 0])
+    current = {s: 0 for s in S}
+    def gsvi_algo(c):
+        # all states
+        S = prop.get_states()
+        # all states that satisfy the goal
+        G = set([s for s in S if prop.get_goal_value(s) == True])
+        if __debug__:
+            assert len(G) > 0
+        # all states that are not safe
+        S0 = set([s for s in S if prop.is_safe(s) == False or len(prop.get_actions(s)) == 0])
 
-    # setting this to 1 for G in the case of reachability properties saves just 1 cycle so -\_(-_-)_/
-    c = {s: 0 for s in S}
+        # setting this to 1 for G in the case of reachability properties saves just 1 cycle so -\_(-_-)_/
+        #c = {s: 0 for s in S}
 
-    # actual value iteration here:
-    for iteration in range(max_iterations):
-        delta = []
-        for s in S:
-            if s in G:
-                nv = 1 if prop.is_probability or prop.is_reachability else 0
-            elif s in S0:
-                nv = 0
-            else:
-                argmax = []
-                for a in prop.get_actions(s):
-                    a_sum = sum([(prop.get_transition_prob(s, a, s_prime) * c[s_prime]) + prop.get_reward(s,a,s_prime) for s_prime in prop.get_next_states(s, a)])
-                    argmax.append(a_sum)
-                nv = prop.get_operation()(argmax)
-            delta.append(abs(nv - c[s]))
-            c[s] = nv
-        if max(delta) < precision:
-            break
-    return PropertyResult(PropertyResultType.FLOAT, c[prop.get_initial_state()], time.time() - timer)
+        # actual value iteration here:
+        for iteration in range(max_iterations):
+            delta = []
+            for s in S:
+                if s in G:
+                    nv = 1 if prop.is_probability or prop.is_reachability else 0
+                elif s in S0:
+                    nv = 0
+                else:
+                    argmax = []
+                    for a in prop.get_actions(s):
+                        a_sum = sum([(prop.get_transition_prob(s, a, s_prime) * c[s_prime]) + prop.get_reward(s,a,s_prime) for s_prime in prop.get_next_states(s, a)])
+                        argmax.append(a_sum)
+                    nv = prop.get_operation()(argmax)
+                delta.append(abs(nv - c[s]))
+                c[s] = nv
+            if max(delta) < precision:
+                return c
+    
+    def ovi(c, e):
+        # all states
+        S = prop.get_states()
+        # all states that satisfy the goal
+        G = set([s for s in S if prop.get_goal_value(s) == True])
+        if __debug__:
+            assert len(G) > 0
+        # all states that are not safe
+        S0 = set([s for s in S if prop.is_safe(s) == False or len(prop.get_actions(s)) == 0])
+        Sq = S - S0 - G
+        c = {s: 0 for s in S}
+        error = e
+        Sq = prop.get_states()
+        while True:
+            c = gsvi_algo(c)
+            u = {s: c[s] * (1 + e) for s in Sq}
+            while True:
+                error = 0
+                up = True
+                down = True
+                cross = False
+                for s in Sq:
+                    v_new = prop.get_operation()([0] + [sum([(prop.get_transition_prob(s, a, s_prime) * c[s_prime]) + prop.get_reward(s,a,s_prime) for s_prime in prop.get_next_states(s, a)]) for a in prop.get_actions(s)])
+                    u_new = prop.get_operation()([0] + [sum([(prop.get_transition_prob(s, a, s_prime) * u[s_prime]) + prop.get_reward(s,a,s_prime) for s_prime in prop.get_next_states(s, a)]) for a in prop.get_actions(s)])
+                    if v_new > 0:
+                        error = max(error, (v_new - c[s])/v_new)
+                    if u_new < u[s]:
+                        up = False
+                    elif u_new > u[s]:
+                        down = False
+                    if u_new < v_new:
+                        cross = True
+                    c[s] = v_new
+                    u[s] = u_new
+                if up or cross:
+                    break
+                elif down and u[prop.get_initial_state()] - c[prop.get_initial_state()] <= 2*e*c[prop.get_initial_state()]:
+                    return 0.5 * (u[prop.get_initial_state()] + c[prop.get_initial_state()])
+            error = error/2
+
+    return PropertyResult(PropertyResultType.FLOAT, ovi(current, precision), time.time() - timer)
