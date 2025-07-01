27594c88d6697122b025853a1670462e4b7c9923
GrootProgrammer <ryan.groot+github@gmail.com>
2025-07-01 10:27:13 +0200

    add json output that can be turned into table

--- combine_results.py
+++ combine_results.py
@@ -6,7 +6,7 @@ results = {}
 print(os.listdir("results"))
 
 for file in os.listdir("results"):
-    if file.endswith(".json") and file != "time_results.json":
+    if file.endswith(".json") and file != "time_results.json" and file != "good_results.json":
         with open(f"results/{file}", "r") as f:
             results[file] = json.load(f)
 
@@ -33,6 +33,36 @@ for file in results:
                 print(f"Results: {results[file][model][algorithm]}")
                 continue
 
-time_results = {k: v for k, v in time_results.items() if len(v) > 0}
+good_results = {}
+
+for file in results:
+    for model in results[file]:
+        if model not in good_results:
+            good_results[model] = {}
+        for algorithm in results[file][model]:
+            if algorithm == "exact":
+                continue
+            try:
+                if any([results[file][model][algorithm][r]["result_type"] in ["not_supported"] for r in results[file][model][algorithm] if r != "total_time"]):
+                    good_results[model][algorithm] = "not supported"
+                    continue
+                if any([results[file][model][algorithm][r]["result_type"] in ["error"] for r in results[file][model][algorithm] if r != "total_time"]):
+                    good_results[model][algorithm] = "error"
+                    continue
+                if any([results[file][model][algorithm][r]["result_type"] in ["timeout"] for r in results[file][model][algorithm] if r != "total_time"]):
+                    good_results[model][algorithm] = "timeout"
+                    continue
+                good_results[model][algorithm] = results[file][model][algorithm]["total_time"]
+            except Exception as e:
+                print(f"Error: {e}")
+                print(f"File: {file}")
+                print(f"Model: {model}")
+                print(f"Algorithm: {algorithm}")
+                print(f"Results: {results[file][model][algorithm]}")
+                continue
+
+good_results = [{"model": k} | v for k, v in good_results.items()]
+
 print(time_results)
 json.dump(time_results, open("results/time_results.json", "w"), indent=4)
+json.dump(good_results, open("results/good_results.json", "w"), indent=4)
